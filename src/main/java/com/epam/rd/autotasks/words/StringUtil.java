9ef11b26f430c99a854d32259e0733b906e04099
snowwhitesheep <snw.wht.lk@gmail.com>
2023-05-15 14:13:59 +0200

    everything except convertPath works

--- src/main/java/com/epam/rd/autotasks/words/StringUtil.java
+++ src/main/java/com/epam/rd/autotasks/words/StringUtil.java
@@ -17,19 +17,34 @@ public class StringUtil {
         }
         return count;
     }
-
+/*
+* Split `text` string into array of words using following separating characters: `",", ".", ";", ":", " ", "?", "!"`.\
+For empty string, `null` string, and string consisting only of separating characters return `null`
+* */
     public static String[] splitWords(String text) {
 
-        //throw new UnsupportedOperationException();
+        //I check if string is empty or null and return null
         if(text == null || text.length()==0){
             return null;
         }
-        String regex = "^[,.;: ?!]*$";
-        Pattern pattern = Pattern.compile(regex);
-        boolean onlyRegex = pattern.matcher(text).matches();
-        if(onlyRegex){return null;}
-        //String textNoWhitespace = text.replaceAll("\\s+", "");
+
+        //I check if string consists only of separating characters and return null
+        String sCharacters = "^[,.;: ?!]*$";
+        Pattern pattern = Pattern.compile(sCharacters);
+        boolean onlySCharacters = pattern.matcher(text).matches();
+        if(onlySCharacters){return null;}
+
+
+        //I split the string
         text = text.trim();
+        StringBuilder snail = new StringBuilder(text);
+        while(snail.charAt(0)==46 || snail.charAt(0)==44
+                ||snail.charAt(0)==59 || snail.charAt(0)==58
+        ||snail.charAt(0)==63 || snail.charAt(0)==33
+        ||snail.charAt(0)==32){
+            snail.deleteCharAt(0);
+        }
+        text = String.valueOf(snail);
         String[] answer = text.split("[,.:;?! ]+");
         for(int i=0; i<answer.length; i++){
             answer[i]= answer[i].trim();
@@ -38,11 +53,86 @@ public class StringUtil {
     }
 
     public static String convertPath(String path, boolean toWin) {
-        throw new UnsupportedOperationException();
+        //I check if the path is null or empty and return null
+        if(path==null || path.isEmpty()){
+            return null;
+        }
+
+        //I check if the path doesn't correspond to any path format (Unix, Windows), return `null`
+        String regex1 = ".*~.*~.*";//I check if there are more than one ~
+        String regex2 = ".*C:.*C:.*";//I check if there are more than one C:
+        String regex3 = ".+C:";//I check if C: is not at the start
+        String regex4 = ".+~";//I check if ~ is not at the start
+        //String regex5 = ".*~.*\\.*";//I check if ~ mixed with \
+        String regex6 = ".*C:.*/.*";//I check if C: mixed with /
+        //String regex7 = ".*\\.*/.*";//I check if \  mixed with /
+
+
+        Pattern pattern1 = Pattern.compile(regex1);
+        Pattern pattern2 = Pattern.compile(regex2);
+        Pattern pattern3 = Pattern.compile(regex3);
+        Pattern pattern4 = Pattern.compile(regex4);
+        //Pattern pattern5 = Pattern.compile(regex5);
+        Pattern pattern6 = Pattern.compile(regex6);
+        //Pattern pattern7 = Pattern.compile(regex7);
+        if(pattern1.matcher(path).matches() || pattern2.matcher(path).matches()
+        || pattern3.matcher(path).matches() || pattern4.matcher(path).matches()
+        //|| pattern5.matcher(path).matches()
+        || pattern6.matcher(path).matches()
+        //|| pattern7.matcher(path).matches()
+        ){
+            return null;
+        }
+
+        //I check if the path already corresponds to the required parameters and return path
+        String regex00 = ".*\\*.*";
+        String regex01 = ".*/*.*";
+        String regex02 = "[^\\/]*";
+        Pattern windows = Pattern.compile(regex00);
+        Pattern unix = Pattern.compile(regex01);
+        Pattern fileName = Pattern.compile(regex02);
+
+        if( (windows.matcher(path).matches() && toWin) || (unix.matcher(path).matches() && !toWin) || fileName.matcher(path).matches() ){
+            return path;
+        }
+
+        String answer = path;
+        if(!toWin){
+            if(answer.startsWith("C:\\User")){
+                answer = answer.replace("C:\\User", "~");
+            }else if(answer.startsWith("C:\\")){
+                answer = answer.replaceFirst("C:\\\\", "/");
+            }
+            answer = answer.replaceAll("\\", "/");
+        }else{
+            if(answer.startsWith("~")){
+                answer = answer.replaceFirst("~", "C:\\User");
+            }else if(answer.startsWith("/")){
+                answer = answer.replace("/", "C:\\");
+            }
+            answer = answer.replaceAll("/", "\\");
+        }
+
+        return answer;
     }
 
     public static String joinWords(String[] words) {
-        throw new UnsupportedOperationException();
+        //throw new UnsupportedOperationException();
+        if(words==null || words.length==0){
+            return null;
+        }
+        String answer = "";
+        for(String element: words){
+            if(!element.isEmpty()){
+                answer+=element+", ";
+            }
+        }
+        if(answer.isEmpty()){
+            return null;
+        }
+        answer = "["+answer.substring(0,answer.length()-2)+"]";
+        return answer;
+
     }
 
     public static void main(String[] args) {
@@ -58,9 +148,10 @@ public class StringUtil {
         String text = "   ,,, first, second!!!! third??";
         String[] splitResult = splitWords(text);
         System.out.println("Result : " + Arrays.toString(splitResult));
-        System.out.println("number of elements -"+splitResult.length);
+        System.out.println("number of elements I have: "+splitResult.length);
         String[] expectedSplit = new String[]{"first", "second", "third"};
         System.out.println("Must be: " + Arrays.toString(expectedSplit));
+        System.out.println("number of elements must be: "+ expectedSplit.length);
 
         System.out.println("Test 3: convertPath");
         String unixPath = "/some/unix/path";
